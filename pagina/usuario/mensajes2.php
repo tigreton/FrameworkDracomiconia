6d9a3a33ea72dd9c45e73d66e43e21079bc2c7d3
bellihood <bellihood@718699e9-cccc-4112-9ac9-6745a42990b9>
2010-02-01 23:34:55 +0000

    Creado el archivo mensajes.php que es el correcto que muestra el id del primer mensaje correctamente. En mensajes2.php intentando hacer un do que recorra todos los mensajes. No sale: expecting `T_FUNCTION in mensajes2.php on line 5

---
+++ pagina/usuario/mensajes2.php
@@ -0,0 +1,27 @@
+<?php
+require_once ('plantillas/pagina.php');
+
+class UsuarioMensajes extends Pagina{
+$nombre=$this->usuario->nombre;
+$hostname_frame = "localhost";
+$database_frame = "dracoframework";
+$username_frame = "root";
+$password_frame = "";
+$frame = mysql_pconnect($hostname_frame, $username_frame, $password_frame); 
+mysql_select_db($database_frame, $frame);
+$query_probando = "SELECT * FROM mensajes WHERE emisario='$nombre'";
+$probando = mysql_query($query_probando, $frame) or die(mysql_error());
+$row_probando = mysql_fetch_assoc($probando);
+
+function content(){
+do {
+return new vParagraph(array(
+   				new vHeader('Mostrando mensajes de: '.$this->usuario->nombre),
+				new vParagraph('Id mensaje'.$row_probando['id']),
+				
+)); 
+}
+while ($row_probando = mysql_fetch_assoc($probando));
+}
+} 	
+?>
