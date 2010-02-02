b9173cf09eab71fb40bcadac1edfa44617e93f0e
bellihood <bellihood@718699e9-cccc-4112-9ac9-6745a42990b9>
2010-02-02 08:18:11 +0000

    Ya no sale error en mensajes2.php Pero solo muestra el ultimo id. Ultimo mensaje.

--- pagina/usuario/mensajes2.php
+++ pagina/usuario/mensajes2.php
@@ -1,27 +1,31 @@
 <?php
 require_once ('plantillas/pagina.php');
 
-class UsuarioMensajes extends Pagina{
+class UsuarioMensajes2 extends Pagina{
+
+	function content(){
+
 $nombre=$this->usuario->nombre;
 $hostname_frame = "localhost";
 $database_frame = "dracoframework";
 $username_frame = "root";
 $password_frame = "";
-$frame = mysql_pconnect($hostname_frame, $username_frame, $password_frame); 
+$frame = mysql_pconnect($hostname_frame, $username_frame, $password_frame);
 mysql_select_db($database_frame, $frame);
 $query_probando = "SELECT * FROM mensajes WHERE emisario='$nombre'";
 $probando = mysql_query($query_probando, $frame) or die(mysql_error());
 $row_probando = mysql_fetch_assoc($probando);
-
-function content(){
 do {
 return new vParagraph(array(
-   				new vHeader('Mostrando mensajes de: '.$this->usuario->nombre),
+				new vHeader('Mostrando mensajes de: '.$this->usuario->nombre),
 				new vParagraph('Id mensaje'.$row_probando['id']),
+				new vParagraph('Quien manda el mensaje: '.$row_probando['emisario']),
 				
 )); 
+} while ($row_probando = mysql_fetch_assoc($probando));
+			
+	}
+
 }
-while ($row_probando = mysql_fetch_assoc($probando));
-}
-} 	
+
 ?>
